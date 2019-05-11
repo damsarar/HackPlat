be707afba57e8f99faf2f6339c6679358d6c2597
damsarar <damsarar@gmail.com>
2019-05-11 12:45:13 +0530

    Second commit

---
+++ app/src/main/java/com/industrialmaster/hackplat/AppConfig.java
@@ -0,0 +1,9 @@
+package com.industrialmaster.hackplat;
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://damsara.tk/hackplat/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://damsara.tk/hackplat/register.php";
+}
